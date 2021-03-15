13db91bf8eb9d6a4eafbee9daa10adf0c97ce7f1
FaroukDev <fa.elayache1@gmail.com>
2021-03-15 14:32:32 +0100

    add reverse test

--- reverse.py
+++ reverse.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 
 def my_function(x):
   return x[::-1]
---
+++ test_revserse.py
@@ -0,0 +1,14 @@
+import unittest
+from reverse import my_function
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def ReverseTestCase(self):
+        self.assertEqual(my_function('hello les noobs'), 'sboon sel olleh')
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
