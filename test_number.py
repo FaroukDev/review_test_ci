baa279a4a5189dcb924fb49b1e5bde0047f457a4
FaroukDev <fa.elayache1@gmail.com>
2021-03-15 14:51:18 +0100

    reverse number test

--- test_number.py
+++ test_number.py
@@ -1,15 +1,11 @@
 import unittest
-from reverse import my_function
 from reverse_number import Convert
 
 
 class TestStringMethods(unittest.TestCase):
-    def ReverseTestCase(self):
-        self.assertEqual(my_function('hello les noobs'), 'sboon sel olleh')
 
     def ReverseNumber(self):
         self.assertEqual(Convert('[2, 5, 8]'), '[-2, -5, -8]')
 
 if __name__ == '__main__':
     unittest.main()
-
